4d54dd0971f4f6c762959995d2f44ea0db201271
Josue Ignacio <dignaciot@miumg.edu.gt>
2024-05-10 14:13:33 -0600

    Javascript

---
+++ JAVASCRIPT/05-objetos/01-intro.js
@@ -0,0 +1,19 @@
+// let email = 'josue@holamundo.io';
+// let name = 'Josue';
+// let direccion = {
+//     calle: 'Jalapa',
+//     numero:15,
+// };
+
+let user = {
+    email: 'josue@holamundo.io',
+    name: 'Josue',
+    direccion: {
+        calle: 'Jalapa',
+        numero: 15,
+    },
+    activo: true,
+    recuperarClave: function() {
+        console.log('recuperando clave...');
+    }
+};
